a2cae55b1ef8f86732e59a17dbdce577fc7c94df
Jozo13 <41787148+Jozo13@users.noreply.github.com>
2023-01-08 06:52:00 +0100

    Added validation

---
+++ app/Http/Requests/MealRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MealRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'page' => 'required|integer|min:1',
+            'per_page' => 'required|integer|min:1',
+            'lang' => 'required|string',
+            'category' => 'integer',
+            'tags' => 'string',
+            'with' => 'string',
+            'diff_time' => 'integer',
+        ];
+    }
+}
